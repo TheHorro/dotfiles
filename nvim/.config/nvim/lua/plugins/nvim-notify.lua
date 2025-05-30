0db9774fc27414e7bbdc0071e715b11a10135be9
Horro <mail@tom-theiss.de>
2025-05-30 22:29:45 +0200

    errormsg fixed

---
+++ nvim/.config/nvim/lua/plugins/nvim-notify.lua
@@ -0,0 +1,8 @@
+return {
+	"rcarriga/nvim-notify",
+	lazy = false, -- or event = "VeryLazy"
+	config = function()
+		require("notify").setup({ background_colour = "#000000" })
+		vim.notify = require("notify")
+	end,
+}
