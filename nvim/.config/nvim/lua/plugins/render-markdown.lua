3ba59c590520583b8334e6fa6b308d374e359512
Horro <mail@tom-theiss.de>
2025-06-17 23:26:31 +0200

    added smear-cursor

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,7 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	enabled = not vim.g.vscode,
+	-- enabled = false,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
